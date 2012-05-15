b6a4fd123d23519f290bd5a3ecfc4bc8f2a58a58
Phil Sung <psung@dnanexus.com>
2012-05-14 17:33:19 -0700

    Initial commit of skeleton code.

---
+++ git_program_builder.py
@@ -0,0 +1,7 @@
+import dxpy
+
+def main():
+    repo_url = job['input']['repo_url']
+    ref = job['input']['ref']
+    program_name = job['input']['program_name']
+    # TODO: obtain credentials and use them to log in
