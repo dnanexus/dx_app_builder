4827f29f402233695721405071efcb7ffba5f646
Phil Sung <psung@dnanexus.com>
2012-05-17 18:22:38 -0700

    Clone a remote repo at a given ref and run dx_build_program on it.

--- git_program_builder.py
+++ git_program_builder.py
@@ -1,7 +1,33 @@
 import dxpy
+import subprocess
+import tempfile
 
 def main():
     repo_url = job['input']['repo_url']
     ref = job['input']['ref']
     program_name = job['input']['program_name']
-    # TODO: obtain credentials and use them to log in
+    # TODO: obtain credentials too and make sure git can read them somehow
+
+    # TODO: pull build tools into worker environment
+
+    print "Repo URL: %s" % (repo_url,)
+    print "Ref name: %s" % (ref,)
+    print "Program name: %s" % (program_name,)
+
+    # Clone the repo and run dx_build_program on it.
+
+    tempdir = tempfile.mkdtemp()
+    print "Working in " + tempdir
+
+    # TODO: protect against directory traversal with program_name
+    # TODO: here or in worker environment, bypass SSH host key prompts
+    os.chdir(tempdir)
+    subprocess.check_call(['git', 'clone', repo_url, program_name])
+
+    os.chdir(program_name)
+    subprocess.check_call(['git', 'checkout', '-q', ref])
+
+    os.chdir(tempdir)
+    subprocess.check_call(['dx_build_program', '--overwrite', program_name])
+
+    shutil.rmtree(tempdir)
