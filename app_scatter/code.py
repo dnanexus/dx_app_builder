a32a126dbc1ba3ca05313282b207a5e14555244d
Ohad Rodeh <orodeh@dnanexus.com>
2018-01-18 15:37:11 -0800

    WIP

--- app_scatter/code.py
+++ app_scatter/code.py
@@ -9,7 +9,7 @@ import json
 @dxpy.entry_point('main')
 def main(**job_inputs):
     # Process input arguments, some of which may be empty
-    executable_id = job_inputs['executable_id']
+    exec_id = job_inputs['exec_id']
     batch_inputs = job_inputs['batch_inputs']
     common_inputs = {}
     if 'common_inputs' in job_inputs:
@@ -24,53 +24,57 @@ def main(**job_inputs):
     # Validate inputs
     # Get a link to the executable, make sure it is one of {app, applet, workflow}
     executable = None
-    if executable_id.startswith("app-"):
-        executable = dxpy.DXApp("'" + executable_id + "'")
-    elif executable_id.startswith("applet-"):
-        executable = dxpy.DXApplet("'" + executable_id + "'")
-    elif executable_id.startswith("workflow-"):
-        executable = dxpy.DXWorkflow("'" + executable_id + "'")
+    if exec_id.startswith("app-"):
+        executable = dxpy.DXApp(exec_id)
+    elif exec_id.startswith("applet-"):
+        executable = dxpy.DXApplet(exec_id)
+    elif exec_id.startswith("workflow-"):
+        executable = dxpy.DXWorkflow(exec_id)
     if executable is None:
-        raise DXError("Executable {} is not an app, applet, or workflow")
+        raise Exception("Executable {} is not an app, applet, or workflow")
 
-    # Get input and output specifications
+    # Get output specification
     output_spec = executable.describe()['outputSpec']
-    output_keys = output_spec.keys()
+    print(output_spec)
+    output_keys = []
+    for d in output_spec:
+        output_keys.append(d["name"])
+    print("output_keys={}".format(output_keys))
 
     # validate batch arguments
     launch_arrays = {}
     width = None
-    if !isinstance(batch_inputs, dict):
-        raise DXError("batch_inputs is not a dictionary type")
+    if not isinstance(batch_inputs, dict):
+        raise Exception("batch_inputs is not a dictionary type")
     for key, arrValues in batch_inputs.iteritems():
-        if !isinstance(arrValues, list):
-            raise DXError("batch_inputs value {} is not a list type".format(arrValues))
-        arr_width = len(arrValues)
+        if not isinstance(arrValues, list):
+            raise Exception("batch_inputs value {} is not a list type".format(arrValues))
+        crnt_width = len(arrValues)
         if width is None:
-            width = arr_width
+            width = crnt_width
         else:
-            if width != arr_width:
-                raise DXError("Input array sizes do not match {} width={} actual_width={}"
-                              .format(key, width, actual_width))
+            if width != crnt_width:
+                raise Exception("Input array sizes do not match {} width={} crnt_width={}"
+                              .format(key, width, crnt_width))
         launch_arrays[key] = arrValues
-    if !isinstance(common_inputs, dict):
-        raise DXError("common_inputs is not a dictionary type")
+    if not isinstance(common_inputs, dict):
+        raise Exception("common_inputs is not a dictionary type")
 
     # validate files argument
-    if !isinstance(files, list):
-        raise DXError("files argument is not a list type")
+    if not isinstance(files, list):
+        raise Exception("files argument is not a list type")
     for f in files:
-        if !isinstance(f, DXFile):
-            raise DXError("{} is not a dx:file".format(f))
+        if not isinstance(f, DXFile):
+            raise Exception("{} is not a dx:file".format(f))
 
     # build dictionary for each invocation
     # TODO: What happens to null values?
     launch_dicts = []
     for i in range(0, width):
         args = {}
-        for k, arrValues = launch_arrays.iteritems():
-            args[k] = arrValues(i)
-        for k, v = common_inputs.iteritems():
+        for k, arrValues in launch_arrays.iteritems():
+            args[k] = arrValues[i]
+        for k, v in common_inputs.iteritems():
             args[k] = v
         launch_dicts.append(args)
 
@@ -88,7 +92,7 @@ def main(**job_inputs):
         for j in jobs:
             out_val  = j.get_output_ref(okey)
             values.append(out_val)
-        output[okey] = values
+        outputs[okey] = values
 
     results = {
         'outputs': outputs,
